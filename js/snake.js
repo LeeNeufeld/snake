bd73e39274efda9784a1ea4b5c35730c737fc1fb
Lee Neufeld <leeneufeld23@hotmail.com>
2020-02-21 15:12:39 -0700

    snake element

--- js/snake.js
+++ js/snake.js
@@ -1,11 +1,16 @@
 var snake;
+var snakeLength;
+var snakeSize;
+
+var food;
 
 var context;
 var screenWidth;
 var screenHeight;
 
 gameInitialize();
-gameDraw();
+snakeInitialize();
+setInterval(gameLoop, 1000 / 30);
 
 function gameInitialize() {
   var canvas = document.getElementById("gameArea");
@@ -17,9 +22,54 @@ function gameInitialize() {
   canvas.height = screenHeight;
 }
 
-function gameLoop() {}
+function gameLoop() {
+  gameDraw();
+  snakeUpdate();
+  snakeDraw();
+}
 
 function gameDraw() {
-  context.fillStyle = "rgb(180, 250, 213)";
+  context.fillStyle = "blue";
   context.fillRect(0, 0, screenWidth, screenHeight);
 }
+
+function snakeInitialize() {
+  snake = [];
+  snakeLength = 5;
+  snakeSize = 16;
+
+  for (var index = snakeLength - 1; index >= 0; index--) {
+    snake.push({
+      x: index,
+      y: 0
+    });
+  }
+}
+
+function snakeDraw() {
+  for (var index = 0; index < snake.length; index++) {
+    context.fillStyle = "white";
+    context.fillRect(
+      snake[index].x * snakeSize,
+      snake[index].y * snakeSize,
+      snakeSize,
+      snakeSize
+    );
+  }
+}
+
+function snakeUpdate() {
+  var snakeX = snake[0].x;
+  var snakeY = snake[0].y;
+
+  snakeX++;
+
+  var snakeTail = snake.pop();
+  snakeTail.x = snakeX;
+  snakeTail.y = snakeY;
+  snake.unshift(snakeTail);
+}
+
+function foodInitialize() {}
+
+function foodDraw() {}
